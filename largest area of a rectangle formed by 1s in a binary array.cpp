b94ceb8b8377b4c3d7a0f14d4dc410b38fd0eb03
gitynity <nitybriliantluckybro@gmail.com>
2020-08-22 00:25:44 +0530

    Trreating 2D matrix as rows of 1D arrays.
    Each row can be solved in a similar way as the largest area of a rectangle in a histogram.

--- largest area of a rectangle formed by 1s in a binary array.cpp
+++ largest area of a rectangle formed by 1s in a binary array.cpp
@@ -7,7 +7,7 @@ vector<int> nsl(vector<int> a)
 	s.push(-1);
 	s.push(0);
 	vector<int> left;
-	left.push_back(-1);	//nothing is in the left array of the leftmost element
+	left.push_back(-1); //nothing is in the left array of the leftmost element
 
 	for (int i = 1; i < a.size(); i++)
 		while (true)
@@ -35,7 +35,7 @@ vector<int> nsl(vector<int> a)
 vector<int> nsr(vector<int> a)
 {
 	stack<int> s;
-	s.push(a.size());	   //a.size()	nothing is in the right array of the rightmost element
+	s.push(a.size());	  //a.size()	nothing is in the right array of the rightmost element
 	s.push(a.size() - 1); //6
 	vector<int> right;
 	right.push_back(a.size());
@@ -64,7 +64,7 @@ vector<int> nsr(vector<int> a)
 	return right;
 }
 
-vector<int> find_area(vector<int> leftlimit , vector<int> rightlimit , vector<int> oneDheights)
+vector<int> find_area(vector<int> leftlimit, vector<int> rightlimit, vector<int> oneDheights)
 {
 	vector<int> lwidth;
 	vector<int> rwidth;
@@ -98,56 +98,52 @@ vector<int> find_area(vector<int> leftlimit , vector<int> rightlimit , vector<in
 		rwidth.push_back(rw);
 	}
 
-	for(int i=0;i<leftlimit.size();i++)
+	for (int i = 0; i < leftlimit.size(); i++)
 	{
-		int temp = lwidth[i] + rwidth[i] -1;
-		area.push_back(temp*oneDheights[i]);
+		int temp = lwidth[i] + rwidth[i] - 1;
+		area.push_back(temp * oneDheights[i]);
 	}
 	return area;
 }
 
-int find_max_area(int a[][5] , int row , int column)	//use and pass vectors to avoid hard setting the size of arrays
+int find_max_area(int a[][5], int row, int column) //use and pass vectors to avoid hard setting the size of arrays
 {
 	vector<int> oneDheights;
-	for(int i=0;i<column;i++)
-		oneDheights.push_back(a[0][i]);	//first row of binary matrix is copied
+	for (int i = 0; i < column; i++)
+		oneDheights.push_back(a[0][i]); //first row of binary matrix is copied
 
 	//apply nsl and nsr on on oneDheights
-	
-	vector<int> templ;	
-	vector<int> tempr;	
+
+	vector<int> templ;
+	vector<int> tempr;
 	vector<int> temp;
-	templ = nsl(oneDheights); 
+	templ = nsl(oneDheights);
 	tempr = nsr(oneDheights);
-	int m=0;
-	for(int i=1;i<row;i++)
-		{
-			for(int j=0;j<column;j++)
-				if(a[i][j]==0)
-					oneDheights[j]=0;
-				else if(a[i][j]==1)
-					oneDheights[j]+=a[i][j];
-			templ = nsl(oneDheights);
-			tempr = nsr(oneDheights);
-			temp = find_area(templ,tempr,oneDheights);
-			m = max(m,*max_element(temp.begin() ,temp.end()));
+	int m = 0;
+	for (int i = 1; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+			if (a[i][j] == 0)
+				oneDheights[j] = 0;
+			else if (a[i][j] == 1)
+				oneDheights[j] += a[i][j];
+		templ = nsl(oneDheights);
+		tempr = nsr(oneDheights);
+		temp = find_area(templ, tempr, oneDheights);
+		m = max(m, *max_element(temp.begin(), temp.end()));
+	}
 
-		}	
-
-
-
-return m;
+	return m;
 }
 
 int main()
 {
 	int binary_matrix[][5] = {
-		{0,1,1,0,1},
-		{1,1,1,1,0},
-		{1,1,1,0,1},
-		{1,1,0,1,0}
-	};
+		{0, 1, 1, 0, 1},
+		{1, 1, 1, 1, 0},
+		{1, 1, 1, 0, 1},
+		{1, 1, 0, 1, 0}};
 
-	int ans = find_max_area(binary_matrix , 5 , 5);
-	cout<<ans;
+	int ans = find_max_area(binary_matrix, 5, 5);
+	cout << ans;
 }
